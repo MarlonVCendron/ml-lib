aad1a21e073eb79dce89a48bbb620849c6902ebb
Marlon <marlonvcendron@gmail.com>
2026-07-16 13:20:24 -0300

    package struct

---
+++ src/linear_regression_gradient_descent/__init__.py
@@ -0,0 +1,4 @@
+from .main import main as package_main
+
+def main() -> None:
+    package_main()
--- src/linear_regression_gradient_descent/linear_regression.py
+++ src/linear_regression_gradient_descent/linear_regression.py
@@ -8,18 +8,28 @@ class LinearRegression:
   _n: int
   _n_coefs: int
   _coefs: npt.NDArray[np.float64]
+  _X: npt.NDArray[np.float64]
+  _y: npt.NDArray[np.float64]
+  _X_base: npt.NDArray[np.float64]
 
   def __init__(self, learning_rate: float = 0.1):
     self._learning_rate = learning_rate
     self._n = 0
     self._n_coefs = 1
-    self._coefs = np.zeros(self._n_coefs)
+    self._coefs = np.zeros(shape=(self._n_coefs))
+    self._X = np.zeros(shape=(self._n_coefs - 1, self._n))
+    self._y = np.zeros(shape=(self._n))
+    self._X_base = np.zeros(shape=(self._n_coefs, self._n))
 
   def fit(self, X: npt.NDArray[np.float64], y: npt.NDArray[np.float64]) -> Self:
     assert X.shape[0] == y.shape[0], 'Shape mismatch between X and y'
 
+    self._X = X
+    self._y = y
+    self._n = X.shape[0]
+    self._X_base = np.c_[np.ones(self._n), self._X] # Add ones for coef_0
+
     self._initialize_coefs(X)
-    self._initialize_n(X)
 
     self._gradient_descent(X, y)
 
@@ -27,9 +37,6 @@ class LinearRegression:
 
   def get_coefs(self) -> npt.NDArray[np.float64]:
     return self._coefs
-
-  def _initialize_n(self, X: npt.NDArray[np.float64]) -> None:
-    self._n = X.shape[0]
 
   def _initialize_coefs(self, X: npt.NDArray[np.float64]) -> None:
     self._n_coefs = X.shape[1] + 1
@@ -49,15 +56,22 @@ class LinearRegression:
         next_coef = self._compute_next_coef(index=i)
         next_coefs[i] = next_coef
 
-      converged = self._has_converged()
+      converged = np.all(next_coefs == self._coefs)
+
+      self._coefs = next_coefs
+
+      print(next_coefs, converged)
 
     return
 
   def _compute_next_coef(self, index: int) -> float:
-    return self._coefs[index] - self._learning_rate * self._cost_function_partial_derivative()
+    return self._coefs[index] - self._learning_rate * self._cost_function_partial_derivative(index)
 
-  def _cost_function_partial_derivative(self) -> float:
-    return 0
+  def _cost_function_partial_derivative(self, index: int) -> float:
+    return (1/self._n) * np.sum(self._X_base.dot(self._hypothesis() - self._y))
+
+  def _hypothesis(self) -> npt.NDArray[np.float64]:
+    return self._X_base.dot(self._coefs)
 
   def _has_converged(self) -> bool:
     return True
